92568e667edc81328c81be4ecf5ac12889e50a66
Josia Menger <josia.menger@zdh.thm.de>
2025-09-16 15:29:52 +0200

    chore: initial commit

---
+++ src/main/java/de/thm/mcpmanagement/McpmanagementApplication.java
@@ -0,0 +1,13 @@
+package de.thm.mcpmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/thm/mcpmanagement/McpmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thm.mcpmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
