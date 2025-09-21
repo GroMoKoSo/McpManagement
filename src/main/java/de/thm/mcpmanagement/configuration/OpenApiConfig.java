bd43406941a3958bd2076e895abc87b258bfff14
Josia Menger <josia.menger@zdh.thm.de>
2025-09-21 22:05:54 +0200

    feat: add OpenAPI documentation
    ref: #1

---
+++ src/main/java/de/thm/mcpmanagement/configuration/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package de.thm.mcpmanagement.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "GroMoKoSo - McpManagement",
+                version = "0.1.0",
+                description = "This API provides endpoints for managing mcp tools",
+                license = @License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                )
+        ),
+        servers = {
+                @Server(url = "${spring.server.base-url}${spring.server.mcp}"),
+        },
+        security = {
+                @SecurityRequirement(name = "keycloak_auth"),
+        }
+)
+@SecurityScheme(
+        name = "oauth2_auth",
+        type = SecuritySchemeType.OAUTH2,
+        description = "OAuth2.0 authentication via Keycloak",
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}/protocol/openid-connect/auth",
+                        tokenUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}/protocol/openid-connect/token"
+                )
+        )
+)
+public class OpenApiConfig {
+}
