e17922c6504c2296c9a88f744e14406ccefc5b26
Josia Menger <josia.menger@zdh.thm.de>
2025-09-26 02:09:46 +0200

    feat: implement mcp service
    ref: #6

---
+++ src/main/java/de/thm/mcpmanagement/service/McpServerServiceImpl.java
@@ -0,0 +1,63 @@
+package de.thm.mcpmanagement.service;
+
+
+import de.thm.mcpmanagement.client.UserManagementClient;
+import de.thm.mcpmanagement.dto.GetApiListResponseDto;
+import de.thm.mcpmanagement.entity.GroMoKoSoMcpServer;
+import de.thm.mcpmanagement.entity.GroMoKoSoMcpServerProvider;
+import de.thm.mcpmanagement.entity.ToolSet;
+import de.thm.mcpmanagement.repository.ToolSetRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+@Service
+public class McpServerServiceImpl implements McpServerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(McpServerServiceImpl.class);
+
+    private final GroMoKoSoMcpServerProvider serverProvider;
+    private final ToolSetRepository toolSetRepository;
+    private final UserManagementClient userManagementClient;
+    private final String version;
+
+    public McpServerServiceImpl(GroMoKoSoMcpServerProvider serverProvider,
+                                ToolSetRepository toolSetRepository,
+                                UserManagementClient userManagementClient,
+                                @Value("${spring.application.version}") String version) {
+        this.serverProvider = serverProvider;
+        this.toolSetRepository = toolSetRepository;
+        this.userManagementClient = userManagementClient;
+        this.version = version;
+    }
+
+    @NonNull
+    @Override
+    @Cacheable(value = "servers", key = "#username")
+    public GroMoKoSoMcpServer getServerForUser(@NonNull String username) {
+        Assert.hasText(username, "username must not be empty");
+
+        logger.info("No server found for user {}. Building a new one", username);
+        List<GetApiListResponseDto> apis = userManagementClient.getApisFromUser(username);
+        var builder = serverProvider.builder().serverInfo("Server of %s".formatted(username), version);
+        int n = 0;
+        for (var api : apis) {
+            ToolSet toolSet = toolSetRepository.findById(api.apiId()).orElse(null);
+            if (toolSet == null) {
+                logger.warn("Tool set with id {} not found, skipping", api.apiId());
+                continue;
+            }
+            builder.addToolSet(toolSet);
+            n++;
+        }
+        logger.info("Created new server for user {} with {} tool sets", username, n);
+        return builder.build();
+    }
+
+}
