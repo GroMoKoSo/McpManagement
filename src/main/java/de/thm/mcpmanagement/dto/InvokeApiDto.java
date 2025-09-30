6465cd9b3331a2aa1fad47822b89a2c6134cdae3
Josia Menger <josia.menger@zdh.thm.de>
2025-10-01 00:27:37 +0200

    fix: update invoke api param names

--- src/main/java/de/thm/mcpmanagement/dto/InvokeApiDto.java
+++ src/main/java/de/thm/mcpmanagement/dto/InvokeApiDto.java
@@ -2,8 +2,8 @@ package de.thm.mcpmanagement.dto;
 
 public record InvokeApiDto(String requestType,
                            String requestPath,
-                           Object headers,
+                           Object header,
                            Object body,
-                           Object requestParameter,
-                           Object pathParameter) { }
+                           Object requestParam,
+                           Object pathParam) { }
 
