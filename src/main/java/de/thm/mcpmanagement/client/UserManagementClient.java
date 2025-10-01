b659d1dbfe27a9b922b0cba033b3a121f38943c0
Josia Menger <josia.menger@zdh.thm.de>
2025-10-01 21:56:57 +0200

    fix: sysadmins that are not part of a group could not add tools to a group

--- src/main/java/de/thm/mcpmanagement/client/UserManagementClient.java
+++ src/main/java/de/thm/mcpmanagement/client/UserManagementClient.java
@@ -5,6 +5,7 @@ import de.thm.mcpmanagement.dto.GetApiListResponseDto;
 import de.thm.mcpmanagement.security.TokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -15,28 +16,13 @@ public class UserManagementClient {
 
     private final TokenProvider tokenProvider;
     private final RestClient client;
-    private final String baseUrl;
 
     public UserManagementClient(TokenProvider tokenProvider,
                                 @Value("${spring.subservices.user-management.url}") String baseUrl) {
         this.tokenProvider = tokenProvider;
-        this.baseUrl = baseUrl;
-        this.client = RestClient.create();
+        this.client = RestClient.create(baseUrl);
     }
 
-    public List<GetApiListResponseDto> getApisFromUserRaw(String username) {
-        try {
-            List<GetApiListResponseDto> apis = client.get()
-                    .uri(baseUrl + "/users/{username}/apis", username)
-                    .header("Authorization", "Bearer " + tokenProvider.getToken())
-                    .retrieve().body(new ParameterizedTypeReference<>() {});
-            if (apis == null)
-                throw new NullPointerException("Request was successful, but request could not be parsed or was null");
-            return apis;
-        } catch (Exception e) {
-            throw ClientExceptionHandler.handleException(e);
-        }
-    }
 
     public List<Integer> getApisFromUser(String username, Boolean active) {
         return getApisFromUserRaw(username)
@@ -48,6 +34,39 @@ public class UserManagementClient {
         for (var api : getApisFromUserRaw(username)) {
             if (api.apiId() == apiId) return api.accessVia();
         }
+        try {
+            for (var api : getAllApisRaw()) {
+                if (api.apiId() == apiId) return api.accessVia();
+            }
+        } catch (Exception ignored) {}
         return null;
+    }
+
+    public List<GetApiListResponseDto> getAllApisRaw() {
+        try {
+            List<GetApiListResponseDto> apis = client.get()
+                    .uri("/users/apis")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenProvider.getToken())
+                    .retrieve().body(new ParameterizedTypeReference<>() {});
+            if (apis == null)
+                throw new NullPointerException("Request was successful, but request could not be parsed or was null");
+            return apis;
+        } catch (Exception e) {
+            throw ClientExceptionHandler.handleException(e);
+        }
+    }
+
+    private List<GetApiListResponseDto> getApisFromUserRaw(String username) {
+        try {
+            List<GetApiListResponseDto> apis = client.get()
+                    .uri("/users/{username}/apis", username)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenProvider.getToken())
+                    .retrieve().body(new ParameterizedTypeReference<>() {});
+            if (apis == null)
+                throw new NullPointerException("Request was successful, but request could not be parsed or was null");
+            return apis;
+        } catch (Exception e) {
+            throw ClientExceptionHandler.handleException(e);
+        }
     }
 }
